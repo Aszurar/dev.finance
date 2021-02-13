1d7e3972b8387284f9ae5fcce81e4257d53707db
Lucas <lms.souza39@gmail.com>
2021-02-12 22:25:16 -0300

    Mais um pouco sobre o dev.finance

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const Modal = {
 // Numbers que representam o dinheiro são tratados diferentes no back-end,
 // eles não possuem a mesma formatação  . ou , Nesse caso, eles são representados
 // com suas partes decimais junto da prinicipal normalmente
-const transaction = [
+const transactions = [
     {
         id: 1,
         description: 'Luz',
@@ -31,6 +31,12 @@ const transaction = [
         id: 3,
         description: 'Internet',
         amount: -20000,
+        date: '23/01/2021'
+    },
+    {
+        id: 4,
+        description: 'App',
+        amount: 240000,
         date: '23/01/2021'
     }
 
@@ -61,17 +67,44 @@ const Transaction = {
 // Ou seja, substituir os dados do HTML com os dados do CSS
 
 const DOM = {
-    addTransaction() {
-        const tr = documet.createElement('tr');
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr');
+        // agr, vamos preencher o tr com a variável html da função abaixo:
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        // agr vamos adicionar todo esse tr à tbody!
+        DOM.transactionsContainer.appendChild(tr)
     },
 
-    innerHTMLTransaction() {
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+        const amount = Utils.formatCurrency(transaction.amount)
+        
         const html =
         `
-            <td class="description">Internet</td>
-            <td class="expense">-R$ 200,00</td>
-            <td class="date">23/01/2021</td>
-            <td><img src="./assets/minus.svg" alt="Remover Transação"></td>
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover Transação">
+            </td>
         `
+
+        return html;
     }
 }
+
+
+const Utils = {
+    formatCurrency(value) {
+        console.log(value);
+    }
+}
+// aqui poderia fazer um tratamento de erro para que caso seja 
+// passado uma transação com um índice maior do que a quantidade
+// que já se tem, a função não prosiga e seja alertado ao usuário
+transactions.forEach(transaction => {
+    DOM.addTransaction(transaction)
+});
+
